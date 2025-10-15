4a1a2dfefdfd5c6e52cb8167bc382c7041fe1ad7
Captain Jack Sparrow <sanketgurav211@gmail.com>
2025-10-15 12:40:24 +0530

    15-10-2025

---
+++ Codeforces/800/432A_Choosing_Teams.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<int>a(n);
+    for(int i=0; i<n; i++) {
+        cin >> a[i];
+    }
+
+    int eligible = 0;
+    for(int i=0; i<n; i++) {
+        if(a[i] + k <= 5) eligible++;
+    }
+
+    cout << eligible/3 << endl;
+    return 0;
+}
