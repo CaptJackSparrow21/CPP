9dcbef89ebf0599b27e80e6f299a985e2a4aac9d
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-03-04 20:11:20 +0530

    04-03-2026

--- Striver/Strivers A2Z DSA Sheet/8. Bit manipulation [concepts & problems]/3. Adanced Maths/Prime factorisation of a Number.cpp
+++ Striver/Strivers A2Z DSA Sheet/8. Bit manipulation [concepts & problems]/3. Adanced Maths/Prime factorisation of a Number.cpp
@@ -3,10 +3,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//TC = O(n √M)      [n queries × √(max query) trials]
+//SC = O(n log M)   [Output size (prime factors count)]
 class Solution {
 public:
+    vector<int> factorize(int x) {
+        vector<int> factors;
+        for(int i=2; i*i<=x; i++) {
+            while(x % i == 0) {
+                factors.push_back(i);
+                x /= i;
+            }
+        }
+        if(x > 1) factors.push_back(x);
+        return factors;
+    }
+
     vector<vector<int>> primeFactors(vector<int> &queries) {
-        
+        int n = queries.size();
+        vector<vector<int>> res(n);
+        for(int i=0; i<n; i++) {
+            res[i] = factorize(queries[i]);
+        }        
+        return res;
     }
 };
 
@@ -30,12 +49,12 @@ int main() {
     }
 
     Solution sol;
-    vector<vector<inht>> ans = sol.primeFactors(queries);
+    vector<vector<int>> ans = sol.primeFactors(queries);
 
     cout << "[";
     for(int i=0; i<ans.size(); i++) {
         cout << "[";
-        for(int j=0; j<ans[i].size; j++) {
+        for(int j=0; j<ans[i].size(); j++) {
             cout << ans[i][j];
             cout << ((j+1) == ans[i].size() ? "" : ",");
         }
