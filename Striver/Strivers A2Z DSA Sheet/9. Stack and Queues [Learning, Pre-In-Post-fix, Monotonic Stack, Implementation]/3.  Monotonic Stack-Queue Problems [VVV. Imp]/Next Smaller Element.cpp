2a32cc2a3ccf42b204bea0a8b957187b0afc8f74
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-03-21 03:26:37 +0530

    21-03-2026

---
+++ Striver/Strivers A2Z DSA Sheet/9. Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/3.  Monotonic Stack-Queue Problems [VVV. Imp]/Next Smaller Element.cpp
@@ -0,0 +1,56 @@
+//https://takeuforward.org/plus/dsa/problems/next-smaller-element?source=strivers-a2z-dsa-track
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+//TC = SC = O(n)
+class Solution {
+public:
+    vector<int> nextSmallerElements(const vector<int> &arr) {
+        int n = arr.size();
+        vector<int> ans(n);
+        stack<int> st;
+
+        for(int i=n-1; i>=0; i--) {
+            while(!st.empty() && st.top() >= arr[i])
+                st.pop();
+
+            ans[i] = st.empty() ? -1 : st.top();
+
+            st.push(arr[i]);
+        }
+        return ans;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string input;
+    getline(cin, input);
+
+    input.erase(remove(input.begin(), input.end(), '['), input.end());
+    input.erase(remove(input.begin(), input.end(), ']'), input.end());
+
+    stringstream ss(input);
+    vector<int> arr;
+    string temp;
+    while(getline(ss, temp, ',')) {
+        if(!temp.empty()) 
+            arr.push_back(stoi(temp));
+    }
+
+    Solution sol;
+    vector<int> ans = sol.nextSmallerElements(arr);
+
+    cout << "[";
+    for(int i=0; i<ans.size(); i++) {
+        cout << ans[i];
+        cout << ((i+1) ==  ans.size() ? "" : ",");
+    }
+    cout << "]";
+
+    return 0;
+}
