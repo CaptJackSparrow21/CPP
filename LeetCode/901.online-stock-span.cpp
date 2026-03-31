78d78e08e442106a2c9d66cc9e20023a190af356
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-03-31 15:12:46 +0530

    31-03-2026

---
+++ LeetCode/901.online-stock-span.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=901 lang=cpp
+ *
+ * [901] Online Stock Span
+ */
+
+// @lc code=start
+class StockSpanner {
+public:
+    stack<pair<int, int>> st;
+
+    StockSpanner() {
+    }
+    
+    int next(int price) {
+        int span = 1;
+        while(!st.empty() && st.top().first <= price) {
+            span += st.top().second;
+            st.pop();
+        }
+
+        st.push({price, span});
+        return span;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
+// @lc code=end
+
