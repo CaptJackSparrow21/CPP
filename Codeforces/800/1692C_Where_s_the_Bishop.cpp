075329041eb21d9c0eccfb5cddb5d336b28cd353
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-04-01 07:04:25 +0530

    01-04-026

---
+++ Codeforces/800/1692C_Where_s_the_Bishop.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while(t--) {
+        vector<string> a(8);
+        for(int i=0; i<8; i++) cin >> a[i];
+
+        for(int i=1; i<7; i++) {
+            for(int j=1; j<7; j++) {
+                if(a[i-1][j-1] == '#' &&
+                   a[i-1][j+1] == '#' &&
+                   a[i+1][j-1] == '#' &&
+                   a[i+1][j+1] == '#')
+                    cout << i+1 << " " << j+1 << endl;
+            }
+        }
+    }
+
+    return 0;
+}
