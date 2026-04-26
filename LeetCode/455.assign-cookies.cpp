0f1a5e53469eb46260c68c27569e0fd34b959ce4
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-04-26 22:03:34 +0530

    26-04-2026

---
+++ LeetCode/455.assign-cookies.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=455 lang=cpp
+ *
+ * [455] Assign Cookies
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int i=0, j=0, cnt = 0;
+        while(i<g.size() && j<s.size()) {
+            if(s[j] >= g[i]) {
+                cnt++;
+                i++;
+            }
+            j++;
+        }
+        return cnt;
+    }
+};
+// @lc code=end
+
