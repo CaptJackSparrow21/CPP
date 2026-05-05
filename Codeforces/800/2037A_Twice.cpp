4792ec62cbff6fd4d7ebe1546c9117c4b4b15011
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-05-05 09:17:39 +0530

    05-05-2026

---
+++ Codeforces/800/2037A_Twice.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        unordered_map<int, int> freq;
+        for(int i=0; i<n; i++) {
+            int x;
+            cin >> x;
+
+            freq[x]++;
+        }
+
+        int ans = 0;
+        for(auto &it : freq)
+            ans += it.second / 2;
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
