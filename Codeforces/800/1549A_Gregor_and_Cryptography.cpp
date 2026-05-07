be8b444e7bed545d165688862b49257b575d08a5
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-05-07 19:58:47 +0530

    07-05-2026

---
+++ Codeforces/800/1549A_Gregor_and_Cryptography.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while(t--) {
+        ll p; cin >> p;
+        cout << 2 << ' ' << p - 1 << endl;
+        
+    }
+
+    return 0;
+}
