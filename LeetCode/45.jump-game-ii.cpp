41c92aad54db2c37c82e431ab8b091053e067ee2
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-05-08 12:16:03 +0530

    08-05-2026

---
+++ LeetCode/45.jump-game-ii.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=45 lang=cpp
+ *
+ * [45] Jump Game II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jumps = 0, end = 0, farthest = 0;
+
+        for(int i=0; i<nums.size()-1; i++) {
+            farthest = max(farthest, i+nums[i]);
+
+            if(i == end) {
+                jumps++;
+                end = farthest;
+            }
+        }
+        return jumps;
+    }
+};
+// @lc code=end
+
