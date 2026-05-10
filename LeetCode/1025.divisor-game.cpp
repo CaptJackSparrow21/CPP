f1a8dec0265d41ed169773361d62a6d008e87f49
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-05-10 12:15:58 +0530

    10-05-2026

---
+++ LeetCode/1025.divisor-game.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=1025 lang=cpp
+ *
+ * [1025] Divisor Game
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool divisorGame(int n) {
+        if(n % 2 == 0)
+            return true;
+        else 
+            return false;
+    }
+};
+// @lc code=end
+
