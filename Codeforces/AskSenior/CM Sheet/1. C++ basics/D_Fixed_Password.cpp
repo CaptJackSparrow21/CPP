9495194774a551912a6a2fb11aa3d31f7b8ea2be
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-05-12 00:46:37 +0530

    12-05-2026

---
+++ Codeforces/AskSenior/CM Sheet/1. C++ basics/D_Fixed_Password.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    while(cin >> n) {
+        if(n != 1999)
+            cout << "Wrong" << endl;
+        else {
+            cout << "Correct" << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
