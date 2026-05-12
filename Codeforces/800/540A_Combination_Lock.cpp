0d7d2bf93ca099940bc275ea9139618f8e4e671d
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-05-13 03:06:11 +0530

    13-05-2026

---
+++ Codeforces/800/540A_Combination_Lock.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n; cin >> n;
+        string x, y;
+        cin >> x >> y;
+
+    int total = 0;
+    for(int i=0; i<n; i++) {
+        int diff = abs(x[i] - y[i]);
+        int rnd = 10 - diff;
+            
+        total += min(diff, rnd);
+    }
+    cout << total << endl;
+    
+
+    return 0;
+}
