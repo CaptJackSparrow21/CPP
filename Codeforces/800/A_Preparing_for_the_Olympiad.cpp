fe80b0a51529ba74da643aef82465107d42dbd41
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-05-14 23:15:00 +0530

    14-05-2026

---
+++ Codeforces/800/A_Preparing_for_the_Olympiad.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+
+        vector<int> a(n), b(n);
+
+        for(int &x : a) cin >> x;
+        for(int &x : b) cin >> x;
+
+        int ans = a[n-1];
+
+        for(int i=0; i<n-1; i++) {
+            ans += max(0, a[i] - b[i + 1]);
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
