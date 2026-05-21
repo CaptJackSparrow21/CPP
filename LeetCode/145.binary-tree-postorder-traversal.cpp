8b50c971b04b20fdc07d4bc78a089ff5352f698c
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-05-21 18:02:10 +0530

    20-05-2026

---
+++ LeetCode/145.binary-tree-postorder-traversal.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=145 lang=cpp
+ *
+ * [145] Binary Tree Postorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void solve(TreeNode *root, vector<int> &ans) {
+        if(root == nullptr)
+            return;
+
+        solve(root->left, ans);
+        solve(root->right, ans);
+        ans.push_back(root->val);
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        solve(root, ans);
+        return ans;
+    }
+};
+// @lc code=end
+
