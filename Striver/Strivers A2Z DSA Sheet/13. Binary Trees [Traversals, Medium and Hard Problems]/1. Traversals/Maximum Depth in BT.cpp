ec3a4a61bcd5e21ffb390f6a01c6fd33d5d73706
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-05-22 09:30:34 +0530

    22-05-2026

---
+++ Striver/Strivers A2Z DSA Sheet/13. Binary Trees [Traversals, Medium and Hard Problems]/1. Traversals/Maximum Depth in BT.cpp
@@ -0,0 +1,82 @@
+//https://takeuforward.org/plus/dsa/problems/maximum-depth-in-bt?source=strivers-a2z-dsa-track
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+//TC = SC = O(n)
+struct TreeNode {
+    int data;
+    TreeNode *left, *right;
+    TreeNode(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode *root) {
+        if(root == nullptr)
+            return 0;
+
+        int leftDepth = maxDepth(root->left);
+        int rightDepth = maxDepth(root->right);
+        return 1 + max(leftDepth, rightDepth);
+    }
+};
+
+TreeNode *buildTree(vector<string> &arr) {
+    if(arr.size() == 0 || arr[0] == "null")
+        return nullptr;
+
+    TreeNode *root = new TreeNode(stoi(arr[0]));
+    queue<TreeNode *> q;
+    q.push(root);
+
+    int i = 1;
+    while(!q.empty() && i < arr.size()) {
+        TreeNode *curr = q.front();
+        q.pop();
+
+        if(i < arr.size() && arr[i] != "null") {
+            curr->left = new TreeNode(stoi(arr[i]));
+            q.push(curr->left);
+        }
+        i++;
+
+        if(i < arr.size() && arr[i] != "null") {
+            curr->right = new TreeNode(stoi(arr[i]));
+            q.push(curr->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s;
+    getline(cin, s);
+
+    vector<string> arr;
+    string temp = "";
+
+    for(char c : s) {
+        if(c == '[' || c == ']')
+            continue;
+        
+        if(c == ',') {
+            arr.push_back(temp);
+            temp = "";
+        }
+        else 
+            temp += c;
+    }
+    arr.push_back(temp);
+
+    TreeNode *root = buildTree(arr);
+    Solution sol;
+    cout << sol.maxDepth(root);
+
+    return 0;
+}
