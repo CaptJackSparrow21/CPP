995140a5ea42b522730c956d4222933b36e6dc4f
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-05-25 11:31:05 +0530

    25-05-2026

---
+++ Codeforces/800/2179A_Blackslex_and_Password.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t; cin >> t;
+    while(t--) {
+        int k, x;
+        cin >> k >> x;
+
+        cout << (k * x) + 1 << endl;
+    }
+
+    return 0;
+}
