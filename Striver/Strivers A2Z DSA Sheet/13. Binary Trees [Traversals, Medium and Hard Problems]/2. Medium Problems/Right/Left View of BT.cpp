eb5c1673d9823d6e645e28f525e3f9528ba7b72d
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-05-26 15:36:23 +0530

    26-05-2026

---
+++ Striver/Strivers A2Z DSA Sheet/13. Binary Trees [Traversals, Medium and Hard Problems]/2. Medium Problems/Right/Left View of BT.cpp
@@ -0,0 +1,99 @@
+//https://takeuforward.org/plus/dsa/problems/right-left-view-of-bt?source=strivers-a2z-dsa-track
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+//TC = SC = O(n)
+struct TreeNode {
+    int data;
+    TreeNode *left, *right;
+    TreeNode(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    void solve(TreeNode *root, int level, vector<int> &ans) {
+        if(root == nullptr)
+            return;
+
+        if(level == ans.size())
+            ans.push_back(root->data);
+
+        solve(root->right, level + 1, ans);
+        solve(root->left, level + 1, ans);
+    }
+
+    vector<int> rightSideView(TreeNode *root) {
+        vector<int> ans;
+        solve(root, 0, ans);
+        return ans;
+    }
+};
+
+TreeNode *buildTree(vector<string> &arr) {
+    if(arr.size() == 0 || arr[0] == "null")
+        return 0;
+
+    TreeNode *root = new TreeNode(stoi(arr[0]));
+    queue<TreeNode *> q;
+    q.push(root);
+
+    int i = 1;
+    while(!q.empty() && i < arr.size()) {
+        TreeNode *curr = q.front();
+        q.pop();
+
+        if(i < arr.size() && arr[i] != "null") {
+            curr->left = new TreeNode(stoi(arr[i]));
+            q.push(curr->left);
+        }
+        i++;
+
+        if(i < arr.size() && arr[i] != "null") {
+            curr->right = new TreeNode(stoi(arr[i]));
+            q.push(curr->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    string s;
+    getline(cin, s);
+
+    vector<string> arr;
+    string temp = "";
+
+    for(char c : s) {
+        if(c == '[' || c == ']')
+            continue;
+
+        if(c == ',') {
+            arr.push_back(temp);
+            temp = "";
+        }
+        else 
+            temp += c;
+    }
+    arr.push_back(temp);
+
+    TreeNode *root = buildTree(arr);
+    
+    Solution sol;
+    vector<int> ans = sol.rightSideView(root);
+
+    cout << "[";
+    for(int i=0; i<ans.size(); i++) {
+        cout << ans[i];
+        cout << ((i+1) == ans.size() ? "" : ",");
+    }
+    cout << "]";
+
+    return 0;
+}
