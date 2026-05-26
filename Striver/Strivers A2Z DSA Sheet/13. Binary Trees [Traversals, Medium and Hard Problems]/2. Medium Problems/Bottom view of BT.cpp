4473adfaf8c9f29ad87ce548178d0630c833825c
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-05-26 18:03:40 +0530

    26-05-2026

---
+++ Striver/Strivers A2Z DSA Sheet/13. Binary Trees [Traversals, Medium and Hard Problems]/2. Medium Problems/Bottom view of BT.cpp
@@ -0,0 +1,117 @@
+//https://takeuforward.org/plus/dsa/problems/bottom-view-of-bt?source=strivers-a2z-dsa-track
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+//TC = O(n log n) && SC = O(n)
+struct TreeNode {
+    int data;
+    TreeNode *left, *right;
+    TreeNode(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    vector<int> bottomView(TreeNode *root) {
+        vector<int> ans;
+        if(root == NULL)
+            return ans;
+
+        //hd -> node value
+        map<int, int> mp;
+
+        //node, hd
+        queue<pair<TreeNode *, int>> q;
+
+        q.push({root, 0});
+
+        while(!q.empty()) {
+            auto it = q.front();
+            q.pop();
+
+            TreeNode *node = it.first;
+            int hd = it.second;
+
+            mp[hd] = node->data;
+
+            if(node->left)
+                q.push({node->left, hd - 1});
+
+            if(node->right)
+                q.push({node->right, hd + 1});
+        }
+
+        for(auto it : mp)
+            ans.push_back(it.second);
+
+        return ans;
+    }
+};
+
+TreeNode *buildTree(vector<string> &arr) {
+    if(arr.size() == 0 || arr[0] == "null")
+        return 0;
+
+    TreeNode *root = new TreeNode(stoi(arr[0]));
+    queue<TreeNode *> q;
+    q.push(root);
+
+    int i = 1;
+    while(!q.empty() && i < arr.size()) {
+        TreeNode *curr = q.front();
+        q.pop();
+
+        if(i < arr.size() && arr[i] != "null") {
+            curr->left = new TreeNode(stoi(arr[i]));
+            q.push(curr->left);
+        }
+        i++;
+
+        if(i < arr.size() && arr[i] != "null") {
+            curr->right = new TreeNode(stoi(arr[i]));
+            q.push(curr->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    string s;
+    getline(cin, s);
+
+    vector<string> arr;
+    string temp = "";
+
+    for(char c : s) {
+        if(c == '[' || c == ']')
+            continue;
+        
+        if(c == ',') {
+            arr.push_back(temp);
+            temp = "";
+        }
+        else    
+            temp += c;
+    }
+    arr.push_back(temp);
+
+    TreeNode *root = buildTree(arr);
+    Solution sol;
+    vector<int> ans = sol.bottomView(root);
+
+    cout << "[";
+    for(int i=0; i<ans.size(); i++) {
+        cout << ans[i];
+        if(i != ans.size() - 1)
+            cout << ",";
+    }
+    cout << "]";
+
+    return 0;
+}
