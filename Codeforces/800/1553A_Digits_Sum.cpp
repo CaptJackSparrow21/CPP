43efaffdd5e98433f3fe6998fe61cfdf81433294
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-05-29 13:15:11 +0530

    29-05-2026

---
+++ Codeforces/800/1553A_Digits_Sum.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        cout << (n + 1) / 10 << endl;
+    }
+
+    return 0;
+}
