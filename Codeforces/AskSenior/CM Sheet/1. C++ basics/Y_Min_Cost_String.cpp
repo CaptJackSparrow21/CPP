262c8b37e29606fbe38472c5ef6d74188a688ea9
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-06-06 14:00:01 +0530

    2026-06-06 14:00:01

--- Codeforces/AskSenior/CM Sheet/1. C++ basics/Y_Min_Cost_String.cpp
+++ Codeforces/AskSenior/CM Sheet/1. C++ basics/Y_Min_Cost_String.cpp
@@ -8,10 +8,29 @@ signed main() {
     cout.tie(0);
 
     string s; cin >> s;
-    vector<int> a(26);
-    for(int &i : a) cin >> i;
+    int n = s.size();
+    int cost[26];
+    for(int i=0; i<26; i++)
+        cin >> cost[i];
 
-    
+    vector<int> right_char(n, -1);
+    int r = -1;
+    for(int i=n-1; i>=0; i--) {
+        if(s[i] != '?')
+            r = i;
+        right_char[i] = r;
+    }
+
+    for(int i=0; i<n; i++) {
+        if(s[i] == '?') {
+            int lc = (i > 0) ? cost[s[i-1] - 'a'] : -1;
+            int ri = right_char[i];
+            int rc = (ri != -1) ? cost[s[ri] - 'a'] : -1;
+
+            char best = 0;
+            int best_
+        }
+    }
 
     return 0;
 }
