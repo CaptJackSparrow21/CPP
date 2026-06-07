52ede5495bad64b1a1cca394b102869c3cc92390
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-06-07 12:30:06 +0530

    2026-06-07 12:30:04

--- LeetCode/2196.create-binary-tree-from-descriptions.cpp
+++ LeetCode/2196.create-binary-tree-from-descriptions.cpp
@@ -19,7 +19,34 @@
 class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        
+        unordered_map<int, TreeNode *> mp;
+        unordered_set<int> childs;
+
+        for(auto &d : descriptions) {
+            int parent = d[0];
+            int child = d[1];
+            int isLeft = d[2];
+
+            if(!mp.count(parent))
+                mp[parent] = new TreeNode(parent);
+
+            if(!mp.count(child))
+                mp[child] = new TreeNode(child);
+
+            if(isLeft)
+                mp[parent]->left = mp[child];
+            else    
+                mp[parent]->right = mp[child];
+
+            childs.insert(child);
+        }
+
+        for(auto &p : mp) {
+            if(!childs.count(p.first))
+                return p.second;
+        }
+
+        return nullptr;
     }
 };
 // @lc code=end
