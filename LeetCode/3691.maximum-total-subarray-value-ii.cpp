f9cc966477faa96f52a2b2e9b51e46df52fd9358
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-06-10 10:00:02 +0530

    2026-06-10 10:00:02

--- LeetCode/3691.maximum-total-subarray-value-ii.cpp
+++ LeetCode/3691.maximum-total-subarray-value-ii.cpp
@@ -5,18 +5,52 @@
  */
 
 // @lc code=start
+#define ll long long
+class SparseTable {
+public:
+    vector<vector<int>> Min, Max;
+
+    SparseTable(const vector<int> &num) {
+        size_t n = num.size();
+        int w = bit_width(n);
+        Min.resize(w, vector<int> (n));
+        Max.resize(w, vector<int> (n));
+
+        for(int i=0; i<n; i++)
+            Min[0][i] = Max[0][i] = num[i];
+
+        for(int i=1; i<w; i++)
+            for(int j=0; j+ (1 << i) <=n; j++) {
+                Min[i][j] = min(Min[i-1][j], Min[i-1][j + (1 << (i-1))]);
+                Max[i][j] = max(Max[i-1][j], Max[i-1][j + (1 << (i-1))]);
+            }
+    }
+
+    int query(int left, int right) {
+        int k = bit_width((uint32_t)right - left) - 1;
+        return max(Max[k][left], Max[k][right - (1 << k)]) - 
+               min(Min[k][left], Min[k][right - (1 << k)]);
+    }
+};
+
 class Solution {
 public:
     long long maxTotalValue(vector<int>& nums, int k) {
-        if(nums.size() == 1)
-            return 0;
-        else if(nums.size() == 2)
-            return abs(nums[1] - nums[0]);
+        ll n = nums.size();
+        ll res = 0;
+        SparseTable LUT(nums);
 
-        long long x = *max_element(nums.begin(), nums.end());
-        long long y = *min_element(nums.begin(), nums.end());
+        priority_queue<tuple<int, int, int>> pq;
+        for(int i=0; i<n; i++)
+            pq.emplace(LUT.query(i,n), i, n);
 
-        return 1LL * k * abs(x-y);
+        while(get<0> (pq.top()) && k--) {
+            auto [val, l, r] = pq.top();
+            pq.pop();
+            res += val;
+            pq.emplace(LUT.query(l, r-1), l, r - 1);
+        }
+        return res;
     }
 };
 // @lc code=end
