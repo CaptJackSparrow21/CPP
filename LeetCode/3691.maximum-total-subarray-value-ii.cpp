8703e66d32d01609432c6e52012d7fdb0e10bc6d
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-06-10 09:30:04 +0530

    2026-06-10 09:30:03

--- LeetCode/3691.maximum-total-subarray-value-ii.cpp
+++ LeetCode/3691.maximum-total-subarray-value-ii.cpp
@@ -8,7 +8,15 @@
 class Solution {
 public:
     long long maxTotalValue(vector<int>& nums, int k) {
-        
+        if(nums.size() == 1)
+            return 0;
+        else if(nums.size() == 2)
+            return abs(nums[1] - nums[0]);
+
+        long long x = *max_element(nums.begin(), nums.end());
+        long long y = *min_element(nums.begin(), nums.end());
+
+        return 1LL * k * abs(x-y);
     }
 };
 // @lc code=end
