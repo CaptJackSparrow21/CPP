bce9622427d865edcce8ee2a677dcf1ff60d8334
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-06-10 08:30:02 +0530

    2026-06-10 08:30:01

---
+++ LeetCode/3691.maximum-total-subarray-value-ii.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=3691 lang=cpp
+ *
+ * [3691] Maximum Total Subarray Value II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    long long maxTotalValue(vector<int>& nums, int k) {
+        
+    }
+};
+// @lc code=end
+
