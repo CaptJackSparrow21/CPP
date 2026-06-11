4f029882c1947c79dcc7d7dc581a9b4256820982
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-06-11 12:00:04 +0530

    2026-06-11 12:00:01

---
+++ Codeforces/800/2148B_Lasers.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t; cin >> t;
+    while(t--) {
+        int n, m, x, y;
+        cin >> n >> m >> x >> y;
+        int a[n];
+        for(int &i : a) cin >> i;
+        int b[m];
+        for(int &i : b) cin >> i;
+
+        cout << n + m << '\n';
+    }
+
+    return 0;
+}
---
+++ Codeforces/800/B_Bobritto_Bandito.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t; cin >> t;
+    while(t--) {
+        int n, m, l, r;
+        cin >> n >> m >> l >> r;
+
+        int i = 0, j = 0;
+        if(m % 2 == 0) {
+            
+        }
+        else {
+
+        }
+    }
+
+    return 0;
+}
