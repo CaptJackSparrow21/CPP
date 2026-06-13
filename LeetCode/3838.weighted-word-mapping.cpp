bd0e7037f8c3d3221b88c3bed6926c35362ede27
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-06-13 15:30:03 +0530

    2026-06-13 15:30:01

---
+++ LeetCode/3838.weighted-word-mapping.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=3838 lang=cpp
+ *
+ * [3838] Weighted Word Mapping
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        
+    }
+};
+// @lc code=end
+
