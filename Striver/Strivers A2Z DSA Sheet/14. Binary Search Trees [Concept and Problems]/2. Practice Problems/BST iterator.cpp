cc16fbe89cea0508923bdbf69e2f88548af2f826
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-06-15 14:00:01 +0530

    2026-06-15 14:00:01

--- Striver/Strivers A2Z DSA Sheet/14. Binary Search Trees [Concept and Problems]/2. Practice Problems/BST iterator.cpp
+++ Striver/Strivers A2Z DSA Sheet/14. Binary Search Trees [Concept and Problems]/2. Practice Problems/BST iterator.cpp
@@ -10,6 +10,7 @@ struct TreeNode {
     TreeNode(int val) : left(nullptr), right(nullptr), data(val) {}
 };
 
+//TC = SC = O(h)
 class BSTIterator {
 public:
     stack<TreeNode *> st;
@@ -39,12 +40,104 @@ public:
     }
 };
 
+vector<string> parseOperations(string s) {
+    vector<string> ops;
+    string curr;
+
+    for(char c : s) {
+        if(isalpha(c))
+            curr += c;
+        else {
+            if(!curr.empty()) {
+                ops.push_back(curr);
+                curr.clear();
+            }
+        }
+    }
+    return ops;
+}
+
+vector<string> parseTreeArray(string s) {
+    vector<string> nodes;
+    string curr = "";
+
+    for(char c : s) {
+        if(c == '[' || c == ']')
+            continue;
+
+        if(c == ',') {
+            nodes.push_back(curr);
+            curr = "";
+        }
+        else 
+            curr += c;
+    }
+    nodes.push_back(curr);
+
+    return nodes;
+}
+
+TreeNode *buildTree(vector<string> &nodes) {
+    if(nodes.empty() || nodes[0] == "null")
+        return nullptr;
+
+    TreeNode *root = new TreeNode(stoi(nodes[0]));
+    queue<TreeNode *> q;
+    q.push(root);
+
+    int i = 1;
+    while(!q.empty() && i < nodes.size()) {
+        TreeNode *curr = q.front();
+        q.pop();
+
+        if(i < nodes.size() && nodes[i] != "null") {
+            curr->left = new TreeNode(stoi(nodes[i]));
+            q.push(curr->left);
+        }
+        i++;
+
+        if(i < nodes.size() && nodes[i] != "null") {
+            curr->right = new TreeNode(stoi(nodes[i]));
+            q.push(curr->right);
+        }
+        i++;
+    }
+    return root;
+}
+
 signed main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
-    
+    string opLine, treeLine;
+    getline(cin, opLine);
+    getline(cin, treeLine);
+
+    vector<string> ops = parseOperations(opLine);
+    vector<string> treeNodes = parseTreeArray(treeLine);
+
+    TreeNode *root = buildTree(treeNodes);
+    BSTIterator *it = nullptr;
+    cout << "[";
+    bool first = true;
+    for(string op : ops) {
+        if(op == "BSTIterator") {
+            it = new BSTIterator(root);
+            continue;
+        }
+
+        if(!first)
+            cout << ",";
+
+        if(op == "next")
+            cout << it->next();
+        else    
+            cout << (it->hasNext() ? "true" : "false");
+
+        first = false;
+    }
+    cout << "]";
 
     return 0;
 }
