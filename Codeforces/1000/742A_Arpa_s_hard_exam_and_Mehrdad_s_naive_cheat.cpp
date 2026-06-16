453bdf917e67846212c7b6720b42de7d47fee391
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-06-16 17:30:03 +0530

    2026-06-16 17:30:01

--- Codeforces/1000/742A_Arpa_s_hard_exam_and_Mehrdad_s_naive_cheat.cpp
+++ Codeforces/1000/742A_Arpa_s_hard_exam_and_Mehrdad_s_naive_cheat.cpp
@@ -10,13 +10,13 @@ signed main() {
     int n; cin >> n;
     if(n == 0)
         cout << 1;
-    else if((n % 10) % 4 == 1)
+    else if(n % 4 == 1)
         cout << 8;
-    else if((n % 10) % 4 == 2)
+    else if(n % 4 == 2)
         cout << 4;
-    else if((n % 10) % 4 == 3)
+    else if(n % 4 == 3)
         cout << 2;
-    else if((n % 10) % 4 == 0)
+    else if(n % 4 == 0)
         cout << 6;
 
     return 0;
