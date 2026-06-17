81583dee56f30160ab2a29200e4fe69c68bda17a
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-06-17 19:00:02 +0530

    2026-06-17 19:00:02

---
+++ CSES/1. Introductory Problems/Bit_Strings.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+const int mod = 1e9 + 7;
+
+int binpow(int a, int b, int p) {
+    if(b == 0) return 1;
+    if(b % 2)
+        return a * binpow(a, b-1, p);
+    
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    
+
+    return 0;
+}
