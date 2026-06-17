f9eeba732107312267656179f7dc57b3737a2d0b
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-06-17 15:30:01 +0530

    2026-06-17 15:30:01

--- Striver/Strivers A2Z DSA Sheet/15. Graphs [Concepts & Problems]/1. Learning/Traversal Techniques.cpp
+++ Striver/Strivers A2Z DSA Sheet/15. Graphs [Concepts & Problems]/1. Learning/Traversal Techniques.cpp
@@ -4,16 +4,81 @@
 using namespace std;
 #define int long long
 
+//TC = SC = O(V + E)
 class Solution {
 public:
-    vector<int> dfsOfGraph(int V, vector<vector<int>> edges) {
+    void dfs(int node, vector<int> &vis,
+            vector<vector<int>> &adj, vector<int> &ans) {
+        vis[node] = 1;
+        ans.push_back(node);
 
+        for(int neighb : adj[node]) {
+            if(!vis[neighb])
+                dfs(neighb, vis, adj, ans);
+        }
+    }
+
+    vector<int> dfsOfGraph(int V, vector<vector<int>> edges) {
+        vector<vector<int>> adj(V);
+        for(auto &e : edges) {
+            int u = e[0];
+            int v = e[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<int> vis(V, 0);
+        vector<int> ans;
+
+        dfs(0, vis, adj, ans);
+        return ans;
     }
 
     vector<int> bfsOfGraph(int V, vector<vector<int>> edges) {
+        vector<vector<int>> adj(V);
 
+        for(auto &e : edges) {
+            int u = e[0];
+            int v = e[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<int> vis(V, 0);
+        vector<int> ans;
+        queue<int> q;
+
+        q.push(0);
+        vis[0] = 1;
+
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            ans.push_back(node);
+
+            for(int neighb : adj[node]) {
+                if(!vis[neighb]) {
+                    vis[neighb] = 1;
+                    q.push(neighb);
+                }
+            }
+        }
+        return ans;
     }
 };
+
+void printAns(vector<int> &ans) {
+    cout << "[";
+    for(int i=0; i<ans.size(); i++) {
+        cout << ans[i];
+        if(i+1 < ans.size())
+            cout << ",";
+    }
+    cout << "]";
+}
 
 signed main() {
     ios_base::sync_with_stdio(0);
@@ -44,7 +109,12 @@ signed main() {
     }
 
     Solution sol;
-    vector<int>
+    vector<int> dfsAns = sol.dfsOfGraph(V, edges);
+    vector<int> bfsAns = sol.bfsOfGraph(V, edges);
+
+    printAns(dfsAns);
+    cout << ",";
+    printAns(bfsAns);
 
     return 0;
 }
