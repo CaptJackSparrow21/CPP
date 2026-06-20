2767743c8bfb2948ef2dcfadab89589cf2ef29aa
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-06-20 16:00:03 +0530

    2026-06-20 16:00:03

--- Striver/Strivers A2Z DSA Sheet/15. Graphs [Concepts & Problems]/2. Problems on BFS-DFS/Flood fill algorithm.cpp
+++ Striver/Strivers A2Z DSA Sheet/15. Graphs [Concepts & Problems]/2. Problems on BFS-DFS/Flood fill algorithm.cpp
@@ -4,11 +4,39 @@
 using namespace std;
 #define int long long
 
+//TC = SC = O(n * m)
 class Solution {
 public:
+    void dfs(int r, int c, vector<vector<int>> &image,
+             int oldColor, int newColor) {
+        int n = image.size();
+        int m = image[0].size();
+
+        image[r][c] = newColor;
+
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
+
+        for(int k=0; k<4; k++) {
+            int nr = r + dr[k];
+            int nc = c + dc[k];
+
+            if(nr >= 0 && nr < n && nc >= 0 && nc < m
+                && image[nr][nc] == oldColor) {
+                dfs(nr, nc, image, oldColor, newColor);
+            }
+        }
+    }
+
     vector<vector<int>> floodFill(vector<vector<int>> &image, 
                         int sr, int sc, int newColor) {
 
+        int oldColor = image[sr][sc];
+        if(oldColor == newColor)
+            return image;
+        
+        dfs(sr, sc, image, oldColor, newColor);
+        return image;
     }
 };
 
@@ -45,11 +73,19 @@ signed main() {
     vector<vector<int>> ans = 
                         sol.floodFill(image, sr, sc, newColor);
 
+    cout << '[';
     for(int i=0; i<ans.size(); i++) {
+        cout << '[';
         for(int j=0; j<ans[i].size(); j++) {
-            cout <<
+            cout << ans[i][j];
+            if(j+1 < ans[i].size())
+                cout << ",";
         }
+        cout << ']';
+        if(i + 1 < ans.size()) 
+            cout << ",";
     }
+    cout << ']';
 
     return 0;
 }
