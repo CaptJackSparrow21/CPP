4819fe22dabd5a39481a25cdd100b650ab197cb1
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-06-22 21:00:01 +0530

    2026-06-22 21:00:01

---
+++ Codeforces/1000/A_Theatre_Square.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, m, a;
+    cin >> n >> m >> a;
+
+    cout << min({n, m, a});
+
+    return 0;
+}
