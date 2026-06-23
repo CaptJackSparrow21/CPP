485380566b2007311a59e79206a793e3ffa75be3
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-06-23 19:00:01 +0530

    2026-06-23 19:00:01

--- HackerRank/Python/Finding_the_percentage.py
+++ HackerRank/Python/Finding_the_percentage.py
@@ -5,3 +5,15 @@ from math import *
 
 n = int(input())
 
+student_marks = {}
+
+for _ in range(n) :
+    name, *line = input().split()
+    scores = list(map(float, line))
+    student_marks[name] = scores
+
+query_name = input().strip()
+
+avg = sum(student_marks[query_name]) / len(student_marks[query_name])
+
+print(f"{avg:.2f}")
