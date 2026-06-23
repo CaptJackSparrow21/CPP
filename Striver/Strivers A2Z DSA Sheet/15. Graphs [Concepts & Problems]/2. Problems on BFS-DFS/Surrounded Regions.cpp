e7f08301777a7508dd6cfc2ad10b8306cdb7804a
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-06-23 15:30:01 +0530

    2026-06-23 15:30:01

--- Striver/Strivers A2Z DSA Sheet/15. Graphs [Concepts & Problems]/2. Problems on BFS-DFS/Surrounded Regions.cpp
+++ Striver/Strivers A2Z DSA Sheet/15. Graphs [Concepts & Problems]/2. Problems on BFS-DFS/Surrounded Regions.cpp
@@ -4,12 +4,47 @@
 using namespace std;
 #define int long long
 
+class Solution {
+public:
+    vector<vector<char>> fill(vector<vector<char>> mat) {
+
+    }
+};
+
 signed main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
-    
+    int n, m;
+    cin >> n >> m;
+
+    string s;
+    getline(cin, s);
+    vector<vector<char>> mat;
+    vector<char> curr_row;
+    string temp = "";
+
+    for(char c : s) {
+        if(isalpha(c))
+            temp += c;
+
+        if((c == ',' || c == ']') && !temp.empty()) {
+            curr_row.push_back(temp);
+            temp = "";
+
+            if(c == ']') {
+                mat.push_back(curr_row);
+                curr_row.clear();
+            }
+        }
+    }
+
+    Solution sol;
+    vector<vector<char>> ans = sol.fill(mat);
+
+    cout << "["
+
 
     return 0;
 }
