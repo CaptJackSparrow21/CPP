b19ff4437ab1c09f07500c9b4b62b3a96ed1c3dc
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-06-23 16:00:03 +0530

    2026-06-23 16:00:01

--- Striver/Strivers A2Z DSA Sheet/15. Graphs [Concepts & Problems]/2. Problems on BFS-DFS/Surrounded Regions.cpp
+++ Striver/Strivers A2Z DSA Sheet/15. Graphs [Concepts & Problems]/2. Problems on BFS-DFS/Surrounded Regions.cpp
@@ -4,10 +4,67 @@
 using namespace std;
 #define int long long
 
+//TC = SC = O(n * m)
 class Solution {
 public:
-    vector<vector<char>> fill(vector<vector<char>> mat) {
+    void dfs(int r, int c, 
+             vector<vector<char>> &mat,
+             vector<vector<int>> &vis) {
+        int n = mat.size();
+        int m = mat[0].size();
 
+        vis[r][c] = 1;
+
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
+
+        for(int k=0; k<4; k++) {
+            int nr = r + dr[k];
+            int nc = c + dc[k];
+
+            if(nr >= 0 && nr < n && nc >= 0 && nc < m
+               && !vis[nr][nc] && mat[nr][nc] == 'O')
+                dfs(nr, nc, mat, vis);
+        }
+    }
+
+    vector<vector<char>> fill(vector<vector<char>> mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+
+        vector<vector<int>> vis(n, vector<int> (m, 0));
+
+        //first row
+        for(int j=0; j<m; j++) {
+            if(mat[0][j] == 'O' && !vis[0][j])
+                dfs(0, j, mat, vis);
+        }
+
+        //last row
+        for(int j=0; j<m; j++) {
+            if(mat[n-1][j] == 'O' && !vis[n-1][j])
+                dfs(n-1, j, mat, vis);
+        }
+
+        //first column
+        for(int i=0; i<n; i++) {
+            if(mat[i][0] == 'O' && !vis[i][0])
+                dfs(i, 0, mat, vis);
+        }
+
+        //last column
+        for(int i=0; i<n; i++) {
+            if(mat[i][m-1] == 'O' && !vis[i][m-1])
+                dfs(i, m-1, mat, vis);
+        }
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(mat[i][j] == 'O' &&  !vis[i][j]) 
+                    mat[i][j] = 'X';
+            }
+        }
+        return mat;
     }
 };
 
@@ -16,34 +73,39 @@ signed main() {
     cin.tie(0);
     cout.tie(0);
 
-    int n, m;
-    cin >> n >> m;
-
     string s;
     getline(cin, s);
     vector<vector<char>> mat;
     vector<char> curr_row;
-    string temp = "";
 
     for(char c : s) {
-        if(isalpha(c))
-            temp += c;
-
-        if((c == ',' || c == ']') && !temp.empty()) {
-            curr_row.push_back(temp);
-            temp = "";
+        if(c == 'O' || c == 'X')
+            curr_row.push_back(c);
 
             if(c == ']') {
-                mat.push_back(curr_row);
-                curr_row.clear();
+                if(!curr_row.empty()) {
+                    mat.push_back(curr_row);
+                    curr_row.clear();
+                }
             }
         }
-    }
 
     Solution sol;
     vector<vector<char>> ans = sol.fill(mat);
 
-    cout << "["
+    cout << "[";
+    for(int i=0; i<ans.size(); i++) {
+        cout << "[";
+        for(int j=0; j<ans[0].size(); j++) {
+            cout << "\"" << ans[i][j] << "\"";
+            if(j+1 != ans[0].size())
+                cout << ",";
+        }
+        cout << "]";
+        if(i+1 != ans.size())    
+            cout << ",";
+    }
+    cout << "]";
 
 
     return 0;
