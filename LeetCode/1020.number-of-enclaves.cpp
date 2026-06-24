94b031e286e8359dd424480322e30389cfdbbb73
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-06-24 12:00:02 +0530

    2026-06-24 12:00:01

---
+++ LeetCode/1020.number-of-enclaves.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1020 lang=cpp
+ *
+ * [1020] Number of Enclaves
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        
+    }
+};
+// @lc code=end
+
