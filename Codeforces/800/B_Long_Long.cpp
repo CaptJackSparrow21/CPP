13ab612b12256eaed4a228222d9abcd8bc610b06
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-06-24 11:00:06 +0530

    2026-06-24 11:00:02

---
+++ Codeforces/800/B_Long_Long.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        
+        int sum = 0;
+        int blocks = 0;
+        bool neg = false;
+
+        for(int i=0; i<n; i++) {
+            int x; cin >> x;
+            sum 
+        }
+
+    }
+
+    return 0;
+}
