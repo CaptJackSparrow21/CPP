5df17e9a471f973b64b2a18c666783773f8dc63c
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-06-25 16:00:01 +0530

    2026-06-25 16:00:01

---
+++ LeetCode/3737.count-subarrays-with-majority-element-i.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=3737 lang=cpp
+ *
+ * [3737] Count Subarrays With Majority Element I
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int countMajoritySubarrays(vector<int>& nums, int target) {
+        
+    }
+};
+// @lc code=end
+
