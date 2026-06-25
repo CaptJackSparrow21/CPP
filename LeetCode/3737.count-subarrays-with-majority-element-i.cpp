08cbf205e1dc4ba60a996616c140da6d1bd9974f
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-06-25 16:32:48 +0530

    2026-06-25 16:32:48

--- LeetCode/3737.count-subarrays-with-majority-element-i.cpp
+++ LeetCode/3737.count-subarrays-with-majority-element-i.cpp
@@ -5,10 +5,62 @@
  */
 
 // @lc code=start
+#define ll long long
 class Solution {
 public:
     int countMajoritySubarrays(vector<int>& nums, int target) {
-        
+        ll n = nums.size();
+        ll ans = 0;
+
+        for(ll l=0; l<n; l++) {
+            ll targetCount = 0;
+
+            for(ll r=l; r<n; r++) {
+                if(nums[r] == target)
+                    targetCount++;
+
+                ll len = r - l + 1;
+                if(targetCount > len / 2)
+                    ans++;
+            }
+        }
+
+        // ll n = nums.size();
+        // ll offset = n + 2;
+        // ll size = 2 * n + 5;
+
+        // vector<ll> bit(size + 1, 0);
+
+        // auto update = [&](ll idx) {
+        //     while(idx <= size) {
+        //         bit[idx]++;
+        //         idx += idx & (-idx);
+        //     }
+        // };
+
+        // auto query = [&](ll idx) {
+        //     ll sum = 0;
+        //     while(idx > 0) {
+        //         sum += bit[idx];
+        //         idx -= idx & (-idx);
+        //     }
+        //     return sum;
+        // };
+
+        // ll ans = 0;
+        // ll preifx = 0;
+        // update(offset);
+
+        // for(ll x : nums) {
+        //     if(x == target)
+        //         prefix++;
+        //     else 
+        //         prefix--;
+
+        //     ans += query(prefix + offset - 1);
+        //     update(prefix + offset);
+        // }
+        return ans;
     }
 };
 // @lc code=end
