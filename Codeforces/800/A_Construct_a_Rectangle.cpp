02036fc6285e9947c0a0165542ea6ebbf79d6d39
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-06-26 16:30:05 +0530

    2026-06-26 16:30:02

---
+++ Codeforces/800/A_Construct_a_Rectangle.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t; cin >> t;
+    while(t--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if((a + b + c) % 2 == 1)
+            cout << "NO\n";
+        else {
+            if((a == b + c) || (b == a + c) ||
+               (c == a + b))
+                cout << "YES\n";
+            else if(
+                (a == b && c % 2 == 0) ||
+                (a == c && b % 2 == 0) ||
+                (b == c && a % 2 == 0)
+            )
+                cout << "YES\n";
+            else 
+                cout << "NO\n";
+        }
+    }
+
+    return 0;
+}
