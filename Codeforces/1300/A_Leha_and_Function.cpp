e7e008d322d644b42dbcb041532d7c118a5f463d
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-06-26 15:00:02 +0530

    2026-06-26 15:00:01

--- Codeforces/1300/A_Leha_and_Function.cpp
+++ Codeforces/1300/A_Leha_and_Function.cpp
@@ -7,6 +7,11 @@ signed main() {
     cin.tie(0);
     cout.tie(0);
 
+    int m; cin >> m;
+    vector<int> a(m), b(m);
+    for(int &i : a) cin >> i;
+    for(int &i : b) cin >> i;
+    
     
 
     return 0;
