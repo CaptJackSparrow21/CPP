f24ff4c00ec2e9309c5f8b00f8246f6f2bac6677
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-06-28 21:30:04 +0530

    2026-06-28 21:30:03

---
+++ Codeforces/1000/A_Young_Physicist.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n; cin >> n;
+    int x = 0, y = 0, z = 0;
+    
+    while(n--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        x += a;
+        y += b;
+        z += c;
+    }
+
+    if(x == 0 && y == 0 && z == 0)
+        cout << "YES";
+    else   
+        cout << "NO";
+
+    return 0;
+}
