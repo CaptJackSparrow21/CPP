8d4af2c2c2998fdc1101f351dd7daa8610270f00
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-07-05 15:30:03 +0530

    2026-07-05 15:30:03

---
+++ Codeforces/1000/479A_Expression.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int ans = max({
+        a + b + c,
+        a * b + c,
+        a + b * c,
+        (a + b) * c,
+        a * (b + c),
+        a * b * c
+    });
+
+    cout << ans;
+
+    return 0;
+}
