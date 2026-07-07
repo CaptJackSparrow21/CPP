c109f7d0050a2e090a8af800e238b39badbc7eda
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-07-07 07:30:03 +0530

    2026-07-07 07:30:03

--- LeetCode/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ LeetCode/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -1,6 +1,15 @@
+#define ll long long
 class Solution {
 public:
     long long sumAndMultiply(int n) {
-        
+        ll x = 0, sum = 0;
+        string s = to_string(n);
+        for(char c : s) {
+            int d = c - '0';
+            sum += d;
+            if(d > 0)
+                x = x * 10 + d;
+        }
+        return x * sum;
     }
 };
