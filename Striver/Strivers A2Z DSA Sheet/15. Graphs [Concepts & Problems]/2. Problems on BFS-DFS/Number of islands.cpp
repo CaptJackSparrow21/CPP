deec313ec198b703d450eca5ffcd11ded074a303
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-07-08 16:30:02 +0530

    2026-07-08 16:30:01

---
+++ Striver/Strivers A2Z DSA Sheet/15. Graphs [Concepts & Problems]/2. Problems on BFS-DFS/Number of islands.cpp
@@ -0,0 +1,26 @@
+//https://takeuforward.org/plus/dsa/problems/number-of-islands?source=strivers-a2z-dsa-track
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+//TC = SC = O(n * m)
+class Solution {
+public:
+    int numIslands(vector<vector<char>> &grid) {
+        int n = grid.size(), m = grid[0].size();
+        
+    }
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    string s;
+    getline(cin, s);
+
+
+    return 0;
+}
