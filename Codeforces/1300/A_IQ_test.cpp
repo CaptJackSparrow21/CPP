73f8c2587326036b05fe51ca93e53f7da919d65d
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-07-09 17:00:02 +0530

    2026-07-09 17:00:02

---
+++ Codeforces/1300/A_IQ_test.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n; cin >> n;
+    vector<int> a(n);
+    int even = 0, odd = 0;
+    for(int i=0; i<n; i++) {
+        cin >> a[i];
+
+        if(a[i] % 2) odd++;
+        else even++;
+    }
+
+    if(even > odd) {
+        for(int i=0; i<n; i++) {
+            if(a[i] % 2) {
+                cout << i + 1;
+                break;
+            }
+        }
+    }
+    else {
+        for(int i=0; i<n; i++) {
+            if(a[i] % 2 == 0)
+        }
+    }
+
+    return 0;
+}
