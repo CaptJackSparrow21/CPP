820ac9d5674fe39167261f09a95388be9a579eb2
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-07-12 14:30:03 +0530

    2026-07-12 14:30:01

---
+++ Codeforces/900/1475A_Odd_Divisor.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        if((n & (n-1)) == 0)
+            cout << "NO\n";
+        else 
+            cout << "YES\n";
+    }
+
+    return 0;
+}
