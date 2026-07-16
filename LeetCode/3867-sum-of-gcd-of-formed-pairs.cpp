624f81922434718729a1589487edb3ae52aea42c
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-07-16 16:00:02 +0530

    2026-07-16 16:00:02

--- LeetCode/3867-sum-of-gcd-of-formed-pairs.cpp
+++ LeetCode/3867-sum-of-gcd-of-formed-pairs.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     long long gcdSum(vector<int>& nums) {
-        vector<int> a;
-        a.push_back(nums[0]);
-        for(int i=1; i<nums.size(); i++) {
-            int x = max(nums[i], nums[i-1]);
-            a.push_back(x);
+        int mx = 0;
+        for(int i=0; i<nums.size(); i++) {
+            mx = max(mx, nums[i]);
+            nums[i] = gcd(mx, nums[i]);
         }
-        sort(a.begin(), a.end());
-        int l = 0, r = a.size() - 1;
-        int ans = 0;
+
+        sort(nums.begin(), nums.end());
+        int l = 0, r = nums.size() - 1;
+        long long ans = 0;
+
         while(l < r) {
-            ans += gcd(a[l], a[r]);
-            l++;
-            r--;
+            ans += gcd(nums[l], nums[r]);
+            l++, r--;
         }
+
         return ans;
     }
 };
