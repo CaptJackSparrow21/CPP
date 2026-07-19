1f03dde5bc6f120cd52f0e85b01590f6d5433da3
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-07-19 19:30:02 +0530

    2026-07-19 19:30:01

--- LeetCode/1081-smallest-subsequence-of-distinct-characters.cpp
+++ LeetCode/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     string smallestSubsequence(string s) {
-        
+        vector<int> freq(26);
+        vector<bool> vis(26, false);
+
+        for(char c : s)
+            freq[c - 'a']++;
+
+        string st = "";
+
+        for(char c : s) {
+            freq[c - 'a']--;
+
+            if(vis[c - 'a'])
+                continue;
+
+            while(!st.empty() && st.back() > c &&
+                  freq[st.back() - 'a'] > 0) {
+                vis[st.back() - 'a'] = false;
+                st.pop_back();
+            }
+
+            st += c;
+            vis[c - 'a'] = true;
+        }
+        return st;
     }
 };
