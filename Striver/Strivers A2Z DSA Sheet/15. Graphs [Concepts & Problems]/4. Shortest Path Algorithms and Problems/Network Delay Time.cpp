31d882891ede7862363e2987d06801944de52594
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-07-20 21:00:02 +0530

    2026-07-20 21:00:01

--- Striver/Strivers A2Z DSA Sheet/15. Graphs [Concepts & Problems]/4. Shortest Path Algorithms and Problems/Network Delay Time.cpp
+++ Striver/Strivers A2Z DSA Sheet/15. Graphs [Concepts & Problems]/4. Shortest Path Algorithms and Problems/Network Delay Time.cpp
@@ -4,12 +4,46 @@
 using namespace std;
 #define int long long
 
+//
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>> &times, int n, int k) {
+
+    }
+};
+
 signed main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
+    string s; 
+    getline(cin, s);
     
+    int n, k;
+    cin >> n;
+    cin >> k;
+
+    vector<vector<int>> times;
+    vector<int> row;
+    string temp = "";
+
+    for(char c : s) {
+        if(c >= '0' && c <= '9')
+            temp += c;
+        else if((c == ',' || c == ']') && !temp.empty()) {
+            row.push_back(stoll(temp));
+            temp = "";
+
+            if(c == ']') {
+                times.push_back(row);
+                row.clear();
+            }
+        }
+    }
+
+    Solution sol;
+    cout << sol.networkDelayTime
 
     return 0;
 }
