15cddd1bc285f6e8d3d3a4cec2a28402de3bf4ad
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-07-23 19:30:02 +0530

    2026-07-23 19:30:02

--- LeetCode/3513-number-of-unique-xor-triplets-i.cpp
+++ LeetCode/3513-number-of-unique-xor-triplets-i.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
+        size_t n = nums.size();
         
+        if(n < 3)
+            return n;
+        return 1 << bit_width(n);
     }
 };
