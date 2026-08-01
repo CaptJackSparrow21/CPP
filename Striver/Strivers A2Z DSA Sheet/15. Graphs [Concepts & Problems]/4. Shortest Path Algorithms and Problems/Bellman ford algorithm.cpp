fe1130337a770570bd09b8c2383761686092ef1a
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-08-01 15:30:01 +0530

    2026-08-01 15:30:01

--- Striver/Strivers A2Z DSA Sheet/15. Graphs [Concepts & Problems]/4. Shortest Path Algorithms and Problems/Bellman ford algorithm.cpp
+++ Striver/Strivers A2Z DSA Sheet/15. Graphs [Concepts & Problems]/4. Shortest Path Algorithms and Problems/Bellman ford algorithm.cpp
@@ -4,10 +4,34 @@
 using namespace std;
 #define int long long
 
+//TC = O(V * E) && SC = O(V)
 class Solution {
 public:
     vector<int> bellman_ford(int V, vector<vector<int>> &edges, int S) {
+        const int inf = 1e8;
+        vector<int> dist(V, inf);
+        dist[S] = 0;
 
+        for(int i=1; i<=V-1; i++) {
+            for(auto &e : edges) {
+                int u = e[0];
+                int v = e[1];
+                int w = e[2];
+
+                if(dist[u] != inf && dist[u] + w < dist[v])
+                    dist[v] = dist[u] + w;
+            }
+        }
+
+        for(auto &e : edges) {
+            int u = e[0];
+            int v = e[1];
+            int w = e[2];
+
+            if(dist[u] != inf && dist[u] + w < dist[v])
+                return {-1};
+        }
+        return dist;
     }
 };
 
@@ -20,16 +44,32 @@ signed main() {
     cin.ignore();
 
     string s;
-    geline(cin, s);
+    getline(cin, s);
 
-    int S; cin >> s;
+    int S; cin >> S;
 
     vector<vector<int>> edges;
     vector<int> row;
     string temp = "";
     for(char c : s) {
-        if(c <= )
+        if(c == '-' || isdigit(c))
+            temp += c;
+        else if((c == ',' || c == ']') && !temp.empty()) {
+            row.push_back(stoll(temp));
+            temp = "";
+
+            if(c == ']') {
+                edges.push_back(row);
+                row.clear();
+            }
+        }
     }
+
+    Solution sol;
+    vector<int> ans = sol.bellman_ford(V, edges, S);
+
+    for(int i : ans)
+        cout << i << ' ';
 
     return 0;
 }
---
+++ Striver/Strivers A2Z DSA Sheet/15. Graphs [Concepts & Problems]/4. Shortest Path Algorithms and Problems/Floyd warshall algorithm.cpp
@@ -0,0 +1,65 @@
+//https://takeuforward.org/plus/dsa/problems/floyd-warshall-algorithm?source=strivers-a2z-dsa-track
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+//TC = O(V^3) && SC = O(1)
+class Solution {
+public:
+    void shortestDistance(vector<vector<int>> &matrix) {
+        int n = matrix.size();
+        const int inf = 1e9;
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(matrix[i][j] == -1)
+                    matrix[i][j] = inf;
+            }
+        }
+
+        for(int k=0; k<n; k++) {
+            for(int i=0; i<n; i++) {
+                for(int j=0; j<n; j++) {
+                    if(matrix[i][k] != inf &&
+                       matrix[k][j] != inf) {
+                        matrix[i][j] = min(matrix[i]p)
+                    }
+                }
+            }
+        }
+    }
+};
+
+
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    string s;
+    getline(cin, s);
+
+    vector<vector<int>> matrix;
+    vector<int> row;
+    string temp = "";
+    for(char c : s) {
+        if(c == '-' || isdigit(c))
+            temp += c;
+        else if((c == ',' || c == ']') && !temp.empty()) {
+            row.push_back(stoll(temp));
+            temp = "";
+
+            if(c == ']') {
+                matrix.push_back(row);
+                row.clear();
+            }
+        }
+    }
+
+    Solution sol;
+    sol.shortestDistance(matrix);
+
+    return 0;
+}
