dd8ddbbd23436ad26627a70d4a5c08db49c76fcf
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-08-01 14:00:04 +0530

    2026-08-01 14:00:04

--- LeetCode/1976.number-of-ways-to-arrive-at-destination.cpp
+++ LeetCode/1976.number-of-ways-to-arrive-at-destination.cpp
@@ -8,7 +8,56 @@
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        
+#define int long long
+
+        const int mod = 1e9 + 7;
+        vector<vector<pair<int, int>>> adj(n);
+        for(auto &e : roads) {
+            int u = e[0];
+            int v = e[1];
+            int w = e[2];
+
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w});
+        }
+
+        vector<int> dist(n, LLONG_MAX);
+        vector<int> ways(n, 0);
+
+        priority_queue<
+            pair<int, int>,
+            vector<pair<int, int>>,
+            greater<pair<int, int>>
+            > pq;
+
+        dist[0] = 0;
+        ways[0] = 1;
+
+        pq.push({0, 0});
+
+        while(!pq.empty()) {
+            auto [d, u] = pq.top();
+            pq.pop();
+
+            if(d > dist[u])
+                continue;
+
+            for(auto &[v, w] : adj[u]) {
+                int newDist = d + w;
+                if(newDist < dist[v]) {
+                    dist[v] = newDist;
+                    ways[v] = ways[u];
+
+                    pq.push({dist[v], v});
+                }
+                else if(newDist == dist[v]) {
+                    ways[v] = (ways[v] + ways[u]) % mod;
+                }
+            }
+        }
+        int ans = ways[n-1];
+#undef int
+        return ans;
     }
 };
 // @lc code=end
