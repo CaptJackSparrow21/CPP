d827cd5413a9a0db3f056ee297ff5587e5453c98
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-08-01 12:30:01 +0530

    2026-08-01 12:30:01

---
+++ LeetCode/486.predict-the-winner.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=486 lang=cpp
+ *
+ * [486] Predict the Winner
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool predictTheWinner(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+
