911d54d87242a2ea240aa9f9efe609db526b1e76
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-08-01 17:00:04 +0530

    2026-08-01 17:00:04

--- Striver/Strivers A2Z DSA Sheet/15. Graphs [Concepts & Problems]/5. MinimumSpanningTree-Disjoint Set and Problems/Prim's Algorithm.cpp
+++ Striver/Strivers A2Z DSA Sheet/15. Graphs [Concepts & Problems]/5. MinimumSpanningTree-Disjoint Set and Problems/Prim's Algorithm.cpp
@@ -8,7 +8,36 @@ using namespace std;
 class Solution {
 public:
     int spanningTree(int V, vector<vector<int>> adj[]) {
+        priority_queue<
+            pair<int, int>,
+            vector<pair<int, int>>,
+            greater<pair<int, int>>
+            > pq;
 
+        vector<int> vis(V, 0);
+        pq.push({0, 0});
+
+        int sum = 0;
+
+        while(!pq.empty()) {
+            auto [wt, node] = pq.top();
+            pq.pop();
+
+            if(vis[node])
+                continue;
+
+            vis[node] = 1;
+            sum += wt;
+
+            for(auto &it : adj[node]) {
+                int adjNode = it[0];
+                int edgeWt = it[1];
+
+                if(!vis[adjNode])
+                    pq.push({edgeWt, adjNode});
+            }
+        }
+        return sum;
     }
 };
 
