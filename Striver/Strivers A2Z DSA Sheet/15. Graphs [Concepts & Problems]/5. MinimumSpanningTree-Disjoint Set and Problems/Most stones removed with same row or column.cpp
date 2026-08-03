ff89c355f1d8a47d61df20b2a3371ef56444935d
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-08-03 18:30:03 +0530

    2026-08-03 18:30:03

--- Striver/Strivers A2Z DSA Sheet/15. Graphs [Concepts & Problems]/5. MinimumSpanningTree-Disjoint Set and Problems/Most stones removed with same row or column.cpp
+++ Striver/Strivers A2Z DSA Sheet/15. Graphs [Concepts & Problems]/5. MinimumSpanningTree-Disjoint Set and Problems/Most stones removed with same row or column.cpp
@@ -4,11 +4,60 @@
 using namespace std;
 #define int long long
 
-//
+//TC : O(n α(N)) 
+//SC : O(maxRow + maxCol)
+//α(N) -> inverse Ackermann function
 class Solution {
 public:
-    int maxRemove(vector<vector<int>> &stones, int n) {
+    vector<int> parent, sz;
 
+    DisjointSet(int n) {
+        parent.resize(n);
+        sz.assign(n, 1);
+
+        for(int i=0; i<n; i++)
+            parent[i] = i;
+    }
+
+    int findPar(int node) {
+        if(node == parent[node])
+            return node;
+        return parent[node] = findPar(parent[node]);
+    }
+
+    void unionBySize(int u, int v) {
+        u = findPar(u);
+        v = findPar(v);
+
+        if(u == v) return;
+
+        if(sz[u] < sz[v])
+            swap(u, v);
+
+        parent[v] = u;
+        sz[u] += sz[v];
+    }
+
+    int maxRemove(vector<vector<int>> &stones, int n) {
+        int maxRow = 0, maxCol = 0;
+
+        for(auto &it : stones) {
+            maxRow = max(maxRow, it[0]);
+            maxCol = max(maxCol, it[1]);
+        }
+
+        DisjointSet ds(maxRow + maxCol + 2);
+
+        unordered_set<int> usedNodes;
+
+        for(auto &it : stones) {
+            int rowNode = it[0];
+            int colNode = it[1] + maxRow + 1;
+
+            ds.unionBySize(rowNode, colNode);
+
+            usedNodes.insert()
+        }
     }
 };
 
