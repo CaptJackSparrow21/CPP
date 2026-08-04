3b9bd8bcbe0bd03ea0fe2a2186f22e14f37c673d
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-08-04 21:00:04 +0530

    2026-08-04 21:00:04

--- Striver/Strivers A2Z DSA Sheet/15. Graphs [Concepts & Problems]/5. MinimumSpanningTree-Disjoint Set and Problems/Accounts merge.cpp
+++ Striver/Strivers A2Z DSA Sheet/15. Graphs [Concepts & Problems]/5. MinimumSpanningTree-Disjoint Set and Problems/Accounts merge.cpp
@@ -7,8 +7,31 @@ using namespace std;
 //TC = O(n log n) && SC = O(n)
 class Solution {
 public:
+    unordered_map<string, vector<string>> G;
+    unordered_set<string> seen;
+    vector<vector<string>> ans;
+
+    void dfs(string &email) {
+        seen.insert(email);
+        ans.back().push_back(email);
+        for(auto &adjEmail : G[email])
+            if(!seen.count(adjEmail))
+                dfs(adjEmail);
+    }
+
     vector<vector<string>> accountsMerge(vector<vector<string>> accounts) {
-        
+        for(auto &acc : accounts)
+            for(int i=2; i<size(acc); i++)
+                G[acc[i]].push_back(acc[i-1]),
+                G[acc[i-1]].push_back(acc[i]);
+
+        for(auto &acc : accounts)
+            if(!seen.count(acc[1])) {
+                ans.push_back({acc[0]});
+                dfs(acc[1]);
+                sort(begin(ans.back()) + 1, end(ans.back()));
+            }
+        return ans;
     }
 };
 
