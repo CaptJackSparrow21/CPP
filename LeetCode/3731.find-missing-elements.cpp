74de3382d6c3375706e5725b01441f655d5e03c5
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-08-04 10:00:05 +0530

    2026-08-04 10:00:04

---
+++ LeetCode/3731.find-missing-elements.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=3731 lang=cpp
+ *
+ * [3731] Find Missing Elements
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+
