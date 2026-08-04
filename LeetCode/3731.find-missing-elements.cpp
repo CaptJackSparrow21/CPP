72375eb0cc29d8e96c9e56bfe0dd04570cf94d1a
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-08-04 14:30:02 +0530

    2026-08-04 14:30:02

--- LeetCode/3731.find-missing-elements.cpp
+++ LeetCode/3731.find-missing-elements.cpp
@@ -8,7 +8,20 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        
+        vector<int> arr = nums;
+        int mx = *max_element(arr.begin(), arr.end());
+        int mn = *min_element(arr.begin(), arr.end());
+        //sort(arr.begin(), arr.end());
+
+        vector<int> ans;
+        for(int i=mn; i<=mx; i++) {
+            auto it = find(arr.begin(), arr.end(), i);
+            if(it == arr.end())
+                ans.push_back(i);
+        }
+
+        sort(ans.begin(), ans.end());
+        return ans;
     }
 };
 // @lc code=end
