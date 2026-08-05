af6c40dd5099301c5a3f79ad7e0446ce78582991
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-08-05 10:30:02 +0530

    2026-08-05 10:30:01

---
+++ LeetCode/3310.remove-methods-from-project.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=3310 lang=cpp
+ *
+ * [3310] Remove Methods From Project
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        
+    }
+};
+// @lc code=end
+
