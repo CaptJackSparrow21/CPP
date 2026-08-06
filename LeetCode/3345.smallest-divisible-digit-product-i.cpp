76894d788bc31fd49e8703cd95fcd7e598e5b7e4
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-08-06 20:00:03 +0530

    2026-08-06 20:00:02

---
+++ LeetCode/3345.smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=3345 lang=cpp
+ *
+ * [3345] Smallest Divisible Digit Product I
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+        
+    }
+};
+// @lc code=end
+
