6e5c6ef819edfd9825d2a96797689e5d00eabd6f
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-08-06 22:30:03 +0530

    2026-08-06 22:30:03

---
+++ LeetCode/745.prefix-and-suffix-search.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=745 lang=cpp
+ *
+ * [745] Prefix and Suffix Search
+ */
+
+// @lc code=start
+class WordFilter {
+public:
+    WordFilter(vector<string>& words) {
+        
+    }
+    
+    int f(string pref, string suff) {
+        
+    }
+};
+
+/**
+ * Your WordFilter object will be instantiated and called as such:
+ * WordFilter* obj = new WordFilter(words);
+ * int param_1 = obj->f(pref,suff);
+ */
+// @lc code=end
+
