33edabce1775c434c1c6cb0424fbd04c6e801cca
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-08-07 09:00:06 +0530

    2026-08-07 09:00:06

--- Striver/Strivers A2Z DSA Sheet/15. Graphs [Concepts & Problems]/5. MinimumSpanningTree-Disjoint Set and Problems/Making a large island.cpp
+++ Striver/Strivers A2Z DSA Sheet/15. Graphs [Concepts & Problems]/5. MinimumSpanningTree-Disjoint Set and Problems/Making a large island.cpp
@@ -4,10 +4,12 @@
 using namespace std;
 #define int long long
 
+//TC = SC = O(n^2)
 class Solution {
 public:
-    int dr[] = {-1, 1, 0, 0};
-    int dc[] = {0, 0, -1, 1};
+    int dr[4] = {-1, 1, 0, 0};
+    int dc[4] = {0, 0, -1, 1};
+    int n;
 
     int dfs(int r, int c, int id, 
             vector<vector<int>> &grid) {
@@ -19,12 +21,64 @@ public:
             int nc = c + dc[k];
 
             if(nr >= 0 && nr < n && nc >= 0 && nc < n
-                && grid[nr][nc] == )
+                && grid[nr][nc] == 1) {
+                    size += dfs(nr, nc, id, grid);
+            }
         }
+        return size;
     }
 
     int largestIsland(vector<vector<int>> &grid) {
+        n = grid.size();
+        vector<int> islandSize;
+        int id = 2;
 
+        //color every island
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(grid[i][j] == 1) {
+                    int size = dfs(i, j, id, grid);
+                    islandSize.push_back(size);
+                    id++;
+                }
+            }
+        }
+
+        //no island present
+        if(islandSize.size() == 0)
+            return 1;
+
+        int ans = 0;
+
+        //try converting each zero
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(grid[i][j] == 0) {
+                    set<int> st;
+                    for(int k=0; k<4; k++) {
+                        int nr = i + dr[k];
+                        int nc = j + dc[k];
+
+                        if(nr >= 0 && nr < n &&
+                           nr < n && nc < n &&
+                           grid[nr][nc] > 1) {
+                            st.insert(grid[nr][nc]);
+                        }
+                    }
+
+                    int size = 1;
+                    for(int islandId : st) 
+                        size += islandSize[islandId - 2];
+                        
+                    ans = max(ans, size);
+                }
+            }
+        }
+
+        for(int x : islandSize)
+            ans = max(ans, x);
+        
+        return ans;
     }
 };
 
