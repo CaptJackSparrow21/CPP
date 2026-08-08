f3be225d638dadb1ca68edcdac423b7a52628828
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-08-08 18:30:03 +0530

    2026-08-08 18:30:03

---
+++ LeetCode/1032.stream-of-characters.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=1032 lang=cpp
+ *
+ * [1032] Stream of Characters
+ */
+
+// @lc code=start
+class StreamChecker {
+public:
+    StreamChecker(vector<string>& words) {
+        
+    }
+    
+    bool query(char letter) {
+        
+    }
+};
+
+/**
+ * Your StreamChecker object will be instantiated and called as such:
+ * StreamChecker* obj = new StreamChecker(words);
+ * bool param_1 = obj->query(letter);
+ */
+// @lc code=end
+
