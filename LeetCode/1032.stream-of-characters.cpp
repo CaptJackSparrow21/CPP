b51765c48342700332742130da335368103b915a
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-08-08 20:00:01 +0530

    2026-08-08 20:00:01

--- LeetCode/1032.stream-of-characters.cpp
+++ LeetCode/1032.stream-of-characters.cpp
@@ -7,12 +7,48 @@
 // @lc code=start
 class StreamChecker {
 public:
+    struct Node {
+        Node *child[26]{};
+        bool end = false;
+    };
+
+    Node *root = new Node();
+    string stream;
+
+    void insert(string &s) {
+        Node *curr = root;
+
+        for(int i=s.size() - 1; i>=0; i--) {
+            int x = s[i] - 'a';
+
+            if(!curr->child[x])
+                curr->child[x] = new Node();
+
+            curr = curr->child[x];
+        }
+        curr->end = true;
+    }
+
     StreamChecker(vector<string>& words) {
-        
+        for(auto &word : words)
+            insert(word);
     }
     
     bool query(char letter) {
-        
+        stream += letter;
+        Node *curr = root;
+        for(int i=stream.size() - 1; i>=0; i--) {
+            int x = stream[i] - 'a';
+
+            if(!curr->child[x])
+                return false;
+
+            curr = curr->child[x];
+
+            if(curr->end)
+                return true;
+        }
+        return false;
     }
 };
 
