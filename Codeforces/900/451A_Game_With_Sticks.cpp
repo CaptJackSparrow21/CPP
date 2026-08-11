67c7a104383f1e84acae704d80cdb6d6e12951eb
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-08-11 21:30:05 +0530

    2026-08-11 21:30:03

---
+++ Codeforces/900/451A_Game_With_Sticks.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+    cout << ((min(n, m) % 2) ? "Akshat" : "Malvika");
+
+    return 0;
+}
