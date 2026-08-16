dd300ce129c700a5e693ad333c58009bf22cd3b8
CaptJackSparrow21 <blockbustersanket211@gmail.com>
2026-08-16 19:34:05 +0530

    Auto-sync 2026-08-16 19:34

---
+++ LeetCode/2029.stone-game-ix.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=2029 lang=cpp
+ *
+ * [2029] Stone Game IX
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        
+    }
+};
+// @lc code=end
+
