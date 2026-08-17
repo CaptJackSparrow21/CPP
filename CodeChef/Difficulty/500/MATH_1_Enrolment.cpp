203a93e2ba6530eb6aed28b691157f95a564e8c3
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-08-18 03:00:02 +0530

    2026-08-18 03:00:01

---
+++ CodeChef/Difficulty/500/MATH_1_Enrolment.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t; cin >> t;
+    while(t--) {
+        int x, y;
+        cin >> x >> y;
+        if(x >= y)
+            cout << "0\n";
+        else    
+            cout << y - x << '\n';
+    }
+
+    return 0;
+}
