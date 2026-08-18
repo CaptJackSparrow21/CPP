6432a4d9306939d6d3bc6c46ab7c913d575c7516
Captain Jack Sparrow <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-08-18 14:30:02 +0530

    2026-08-18 14:30:01

--- LeetCode/3471.find-the-largest-almost-missing-integer.cpp
+++ LeetCode/3471.find-the-largest-almost-missing-integer.cpp
@@ -8,7 +8,32 @@
 class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
-        
+        int n = nums.size();
+        if(n == k)
+            return *max_element(nums.begin(), nums.end());
+
+        if(k == 1) {
+            int cnt[51] = {};
+            for(int x : nums)
+                cnt[x]++;
+
+            int ans = -1;
+            for(int x : nums)
+                if(cnt[x] == 1)
+                    ans = max(ans, x);
+
+            return ans;
+        }
+
+        int ans = -1;
+
+        if(count(nums.begin() + 1, nums.end(), nums[0]) == 0)
+            ans = max(ans, nums[0]);
+
+        if(count(nums.begin(), nums.end() - 1, nums[n-1]) == 0)
+            ans = max(ans, nums[n-1]);
+
+        return ans;
     }
 };
 // @lc code=end
