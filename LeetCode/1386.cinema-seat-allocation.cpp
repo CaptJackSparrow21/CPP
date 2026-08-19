e072437325b08e4b1e364730ee81086b2fb9bae9
CaptJackSparrow21 <blockbustersanket211@gmail.com>
2026-08-19 19:15:11 +0530

    Auto-sync 2026-08-19 19:15

---
+++ LeetCode/1386.cinema-seat-allocation.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1386 lang=cpp
+ *
+ * [1386] Cinema Seat Allocation
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
+        
+    }
+};
+// @lc code=end
+
