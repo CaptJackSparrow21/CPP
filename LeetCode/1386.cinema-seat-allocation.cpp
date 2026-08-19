237442720ce2995a84b7eb434344d3c0db0b2309
CaptJackSparrow21 <blockbustersanket211@gmail.com>
2026-08-19 19:45:16 +0530

    Auto-sync 2026-08-19 19:45

--- LeetCode/1386.cinema-seat-allocation.cpp
+++ LeetCode/1386.cinema-seat-allocation.cpp
@@ -8,7 +8,24 @@
 class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
-        
+        unordered_map<int, int> row;
+
+        for(auto &r : reservedSeats)
+            row[r[0]] |= 1 << (r[1] - 1);
+
+        int ans = 2 * (n - row.size());
+
+        for(auto &[r, mask] : row) {
+            bool left = !(mask & 0b0000011110);
+            bool right = !(mask & 0b0111100000);
+            bool mid = !(mask & 0b0001111000);
+
+            if(left & right)
+                ans += 2;
+            else if(left || mid || right)
+                ans += 1;
+        }
+        return ans;
     }
 };
 // @lc code=end
