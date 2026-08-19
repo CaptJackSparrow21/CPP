a71d7ab828e30e049a2b2b2b80ee4535344b120d
CaptJackSparrow21 <blockbustersanket211@gmail.com>
2026-08-19 20:15:25 +0530

    Auto-sync 2026-08-19 20:15

--- Codeforces/800/A_GamingForces.cpp
+++ Codeforces/800/A_GamingForces.cpp
@@ -7,7 +7,19 @@ signed main() {
     cin.tie(0);
     cout.tie(0);
 
-    
+    int t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        int cnt = 0;
+        for(int i=0; i<n; i++) {
+            int x; cin >> x;
+            if(x == 1)
+                cnt++;
+        }
+
+        int ans = (n - cnt) + (cnt + 1) / 2;
+        cout << ans << '\n';
+    }
 
     return 0;
 }
