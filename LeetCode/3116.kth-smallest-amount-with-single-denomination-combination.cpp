d93908b1a27e8a242fa95aa855b3154d09fae986
CaptJackSparrow21 <blockbustersanket211@gmail.com>
2026-08-21 19:07:50 +0530

    Auto-sync 2026-08-21 19:07

---
+++ LeetCode/3116.kth-smallest-amount-with-single-denomination-combination.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=3116 lang=cpp
+ *
+ * [3116] Kth Smallest Amount With Single Denomination Combination
+ */
+
+// @lc code=start
+class Solution {
+public:
+    long long findKthSmallest(vector<int>& coins, int k) {
+        
+    }
+};
+// @lc code=end
+
