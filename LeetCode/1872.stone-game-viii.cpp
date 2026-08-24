ff271f46c1fd497b7c491802e80677de9cc6a990
CaptJackSparrow21 <blockbustersanket211@gmail.com>
2026-08-24 10:44:47 +0530

    Auto-sync 2026-08-24 10:44

---
+++ LeetCode/1872.stone-game-viii.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1872 lang=cpp
+ *
+ * [1872] Stone Game VIII
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int stoneGameVIII(vector<int>& stones) {
+        
+    }
+};
+// @lc code=end
+
