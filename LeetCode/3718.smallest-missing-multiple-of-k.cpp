4d5b9cea7e67d1085527afc6c52dd8054f1b95b9
CaptJackSparrow21 <blockbustersanket211@gmail.com>
2026-08-25 09:58:42 +0530

    Auto-sync 2026-08-25 09:58

---
+++ LeetCode/3718.smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=3718 lang=cpp
+ *
+ * [3718] Smallest Missing Multiple of K
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        
+    }
+};
+// @lc code=end
+
