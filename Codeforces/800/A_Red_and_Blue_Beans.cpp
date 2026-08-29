72ed56068472c2259cc7aa653ca5478007d15180
CaptJackSparrow21 <blockbustersanket211@gmail.com>
2026-08-29 16:15:56 +0530

    Auto-sync 2026-08-29 16:15

---
+++ Codeforces/800/A_Red_and_Blue_Beans.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t; cin >> t;
+    while(t--) {
+        int r, b, d;
+        cin >> r >> b >> d;
+        
+    }
+
+    return 0;
+}
