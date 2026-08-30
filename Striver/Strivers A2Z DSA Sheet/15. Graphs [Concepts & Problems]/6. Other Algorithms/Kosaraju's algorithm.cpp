ab6dff3bc3d451ca826f35f5eb73026cd65adc3b
CaptJackSparrow21 <blockbustersanket211@gmail.com>
2026-08-30 22:16:38 +0530

    Auto-sync 2026-08-30 22:16

---
+++ Striver/Strivers A2Z DSA Sheet/15. Graphs [Concepts & Problems]/6. Other Algorithms/Kosaraju's algorithm.cpp
@@ -0,0 +1,24 @@
+//https://takeuforward.org/plus/dsa/problems/kosaraju's-algorithm?source=strivers-a2z-dsa-track
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+//
+class Solution {
+public:
+    int kosaraju(int V, vector<int> adj[]) {
+
+    }
+};
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int V; cin >> V;
+    
+
+    return 0;
+}
