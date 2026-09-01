66876839cb3cc21c0f2b800a2252657e75620a17
CaptJackSparrow21 <blockbustersanket211@gmail.com>
2026-09-01 17:21:41 +0530

    Auto-sync 2026-09-01 17:21

--- Striver/Strivers A2Z DSA Sheet/16. Dynamic Programming [Patterns and Problems]/2. 1D DP/Frog Jump.cpp
+++ Striver/Strivers A2Z DSA Sheet/16. Dynamic Programming [Patterns and Problems]/2. 1D DP/Frog Jump.cpp
@@ -4,10 +4,28 @@
 using namespace std;
 #define int long long
 
+//TC = O(n) && SC = O(1)
 class Solution {
 public:
     int frogJump(vector<int> &heights) {
+        int n = heights.size();
 
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for(int i=1; i<n; i++) {
+            int one = prev1 + abs(heights[i] - heights[i-1]);
+            int two = INT_MAX;
+
+            if(i > 1)
+                two = prev2 + abs(heights[i] - heights[i-2]);
+
+            int curr = min(one, two);
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
     }
 };
 
@@ -16,7 +34,23 @@ signed main() {
     cin.tie(0);
     cout.tie(0);
 
-    
+    string s;
+    getline(cin, s);
+
+    s.erase(remove(s.begin(), s.end(), '['), s.end());
+    s.erase(remove(s.begin(), s.end(), ']'), s.end());
+
+    vector<int> heights;
+    stringstream ss(s);
+    string temp;
+    while(getline(ss, temp, ',')) {
+        if(!temp.empty())
+            heights.push_back(stoll(temp));
+    }
+
+    Solution sol;
+    cout << sol.frogJump(heights);
+
 
     return 0;
 }
---
+++ Striver/Strivers A2Z DSA Sheet/16. Dynamic Programming [Patterns and Problems]/2. 1D DP/Frog jump with K distances.cpp
@@ -0,0 +1,39 @@
+//https://takeuforward.org/plus/dsa/problems/frog-jump-with-k-distances?source=strivers-a2z-dsa-track
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+//TC = O(n * k) && SC = O(n)
+class Solution {
+public:
+    int frogJump(vector<int> &heights, int k) {
+
+    }
+};
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    string s;
+    getline(cin, s);
+    int k; cin >> k;
+
+    s.erase(remove(s.begin(), s.end(), '['), s.end());
+    s.erase(remove(s.begin(), s.end(), ']'), s.end());
+
+    vector<int> heights;
+    string temp;
+    stringstream ss(s);
+    while(getline(ss, temp, ',')) {
+        if(!temp.empty())
+            heights.push_back(stoll(temp));
+    }
+
+    Solution sol;
+    cout << sol.frogJump(heights, k);
+
+    return 0;
+}
