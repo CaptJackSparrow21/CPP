7676ca56e448cc841243b819bf676fe9def848d8
CaptJackSparrow21 <blockbustersanket211@gmail.com>
2026-09-11 17:07:32 +0530

    Auto-sync 2026-09-11 17:07

---
+++ LeetCode/3483.unique-3-digit-even-numbers.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=3483 lang=cpp
+ *
+ * [3483] Unique 3-Digit Even Numbers
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+        
+    }
+};
+// @lc code=end
+
