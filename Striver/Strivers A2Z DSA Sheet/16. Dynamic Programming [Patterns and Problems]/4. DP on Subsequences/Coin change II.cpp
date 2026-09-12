2942f0f7f38a631ba8f6c370eb79161613141a24
CaptJackSparrow21 <blockbustersanket211@gmail.com>
2026-09-12 22:18:52 +0530

    Auto-sync 2026-09-12 22:18

---
+++ Striver/Strivers A2Z DSA Sheet/16. Dynamic Programming [Patterns and Problems]/4. DP on Subsequences/Coin change II.cpp
@@ -0,0 +1,31 @@
+//https://takeuforward.org/plus/dsa/problems/coin-change-ii?source=strivers-a2z-dsa-track
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+//
+class Solution {
+public:
+    int count(vector<int> &coins, int N, int amount) {
+
+    }
+};
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    string s;
+    getline(cin, s);
+    int amount; cin >> amount;
+    
+    vector<int> coins;
+    string temp;
+    for(char c : s) {
+        if(c == '-')
+    }
+
+    return 0;
+}
