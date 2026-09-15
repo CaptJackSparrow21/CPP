7b3f5b7eec21c05f759ef8ea6d3ac8b36618c29d
CaptJackSparrow21 <blockbustersanket211@gmail.com>
2026-09-15 15:48:11 +0530

    Auto-sync 2026-09-15 15:48

---
+++ Striver/Strivers A2Z DSA Sheet/16. Dynamic Programming [Patterns and Problems]/5. DP on Strings/Minimum insertions to make string palindrome.cpp
@@ -0,0 +1,27 @@
+//https://takeuforward.org/plus/dsa/problems/minimum-insertions-to-make-string-palindrome?source=strivers-a2z-dsa-track
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+//
+class Solution {
+public:
+    int minInsertion(string s) {
+        int n = s.size();
+        string rev = s;
+        reverse(s.)
+    }
+};
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    string s; cin >> s;
+    Solution sol;
+    cout << sol.minInsertion(s);
+
+    return 0;
+}
