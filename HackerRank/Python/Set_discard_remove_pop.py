3324ac297e19672d0edd5938ff866f8a485c87ee
CaptJackSparrow21 <blockbustersanket211@gmail.com>
2026-09-15 11:17:15 +0530

    Auto-sync 2026-09-15 11:17

--- HackerRank/Python/Set_discard_remove_pop.py
+++ HackerRank/Python/Set_discard_remove_pop.py
@@ -1,3 +1,17 @@
 n = int(input())
 s = set(map(int, input().split()))
-k = int(input)
+k = int(input())
+
+for _ in range(k):
+    cmd = input().split()
+
+    if cmd[0] == "pop":
+        s.remove(min(s))
+
+    elif cmd[0] == "remove":
+        s.remove(int(cmd[1]))
+
+    elif cmd[0] == "discard":
+        s.discard(int(cmd[1]))
+
+print(sum(s))
