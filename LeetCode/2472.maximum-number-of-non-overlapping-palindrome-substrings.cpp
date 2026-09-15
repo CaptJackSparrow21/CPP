8c5b58b6b1e4366b82f2949829b9c040bd53fc44
CaptJackSparrow21 <blockbustersanket211@gmail.com>
2026-09-15 11:47:18 +0530

    Auto-sync 2026-09-15 11:47

---
+++ LeetCode/2472.maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=2472 lang=cpp
+ *
+ * [2472] Maximum Number of Non-overlapping Palindrome Substrings
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxPalindromes(string s, int k) {
+        
+    }
+};
+// @lc code=end
+
