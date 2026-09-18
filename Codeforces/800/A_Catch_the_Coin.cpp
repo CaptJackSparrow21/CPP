42bab9cc5bc51b72a29f95352dc63013b4d4e63c
CaptJackSparrow21 <blockbustersanket211@gmail.com>
2026-09-18 16:32:40 +0530

    Auto-sync 2026-09-18 16:32

---
+++ Codeforces/800/A_Catch_the_Coin.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n; cin >> n;
+    while(n--) {
+        int x, y;
+        cin >> x >> y;
+        
+    }
+
+    return 0;
+}
