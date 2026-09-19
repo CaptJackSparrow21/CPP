6be411714a4db1e9719f49a03f70c9b9f1bbbd10
CaptJackSparrow21 <blockbustersanket211@gmail.com>
2026-09-19 22:22:09 +0530

    Auto-sync 2026-09-19 22:22

--- Striver/Strivers A2Z DSA Sheet/16. Dynamic Programming [Patterns and Problems]/7. DP on LIS/Print Longest Increasing Subsequence.cpp
+++ Striver/Strivers A2Z DSA Sheet/16. Dynamic Programming [Patterns and Problems]/7. DP on LIS/Print Longest Increasing Subsequence.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 #define int long long
 
-//
+//TC = O(n^2) && SC = O(n)
 class Solution {
 public:
     vector<int> longestIncreasingSubsequence(vector<int> &arr) {
@@ -20,17 +20,43 @@ public:
         vector<int> ans;
         int prev = INT_MIN;
 
-        
-        
+        for(int i=0; i<n && len > 0; i++) {
+            if(arr[i] > prev && dp[i] == len) {
+                ans.push_back(arr[i]);
+                prev = arr[i];
+                len--;
+            }
+        }
+        return ans;
     }
-}
+};
 
 signed main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
-    
+    string s;
+    getline(cin, s);
+    vector<int> arr;
+    string temp;
+    for(char c : s) {
+        if(c == '-' || isdigit(c))
+            temp += c;
+        else if((c == ',' || c == ']') && !temp.empty()) {
+            arr.push_back(stoll(temp));
+            temp = "";
+        }
+    }
+
+    Solution sol;
+    vector<int> ans = sol.longestIncreasingSubsequence(arr);
+    cout << '[';
+    for(int i=0; i<ans.size(); i++) {
+        cout << ans[i];
+        cout << (i+1 == ans.size() ? "" : ",");
+    }
+    cout << ']';
 
     return 0;
 }
