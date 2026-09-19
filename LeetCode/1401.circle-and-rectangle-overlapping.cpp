d6702a8c8bfa42ba23d3bf0f6b4063507306fb9c
CaptJackSparrow21 <blockbustersanket211@gmail.com>
2026-09-19 15:59:37 +0530

    Auto-sync 2026-09-19 15:59

---
+++ LeetCode/1401.circle-and-rectangle-overlapping.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1401 lang=cpp
+ *
+ * [1401] Circle and Rectangle Overlapping
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        
+    }
+};
+// @lc code=end
+
