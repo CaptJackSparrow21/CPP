8220a440b607cefaff9b288717d403b3719cdde1
CaptJackSparrow21 <blockbustersanket211@gmail.com>
2026-09-20 16:45:47 +0530

    Auto-sync 2026-09-20 16:45

--- LeetCode/2035.partition-array-into-two-arrays-to-minimize-sum-difference.cpp
+++ LeetCode/2035.partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -8,20 +8,43 @@
 class Solution {
 public:
     int minimumDifference(vector<int>& nums) {
-        int total = accumulate(nums.begin(), nums.end(), 0);
-        vector<bool> dp(total + 1, false);
-        dp[0] = true;
+        int n = nums.size() / 2, total = 0, ans = INT_MAX;
 
-        for(int x : nums) {
-            for(int s = total; s >= x; s--)
-                dp[s] = dp[s] || dp[s-x];
+        for(int x : nums)
+            total += x;
+
+        vector<vector<int>> left(n + 1), right(n + 1);
+        for(int mask=0; mask < (1 << n); mask++) {
+            int cnt = 0, s1 = 0, s2 = 0;
+            for(int i=0; i<n; i++) {
+                if(mask & (1 << i)) {
+                    cnt++;
+                    s1 += nums[i];
+                    s2 += nums[i + n];
+                }
+            }
+            left[cnt].push_back(s1);
+            right[cnt].push_back(s2);
         }
 
-        for(int s=total/2; s>=0; s--)
-            if(dp[s])
-                return total - 2 * s;
+        for(auto &v : right)
+            sort(v.begin(), v.end());
+        
+        for(int cnt=0; cnt<=n; cnt++) {
+            for(int x : left[cnt]) {
+                int target = total / 2 - x;
+                auto it = lower_bound(right[n - cnt].begin(), right[n - cnt].end(), target);
 
-        return total;
+                if(it != right[n - cnt].end())
+                    ans = min(ans, abs(total - 2 * (x + *it)));
+
+                if(it != right[n - cnt].begin()) {
+                    --it;
+                    ans = min(ans, abs(total - 2 * (x + *it)));
+                }
+            }
+        }
+        return ans;
     }
 };
 // @lc code=end
