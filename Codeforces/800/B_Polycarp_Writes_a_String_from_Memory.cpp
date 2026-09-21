5d1bf949fc054a46831789df1d561947b8b06758
CaptJackSparrow21 <blockbustersanket211@gmail.com>
2026-09-21 18:54:14 +0530

    Auto-sync 2026-09-21 18:54

---
+++ Codeforces/800/B_Polycarp_Writes_a_String_from_Memory.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t; cin >> t;
+    while(t--) {
+        string s; cin >> s;
+        int ans = 0;
+        set<char> v;
+        for(int i=0; i<s.size(); i++) {
+            v.insert(s[i]);
+            if()
+        }
+    }
+
+    return 0;
+}
