d0d07ac87af7fd720f0f63dd8eaf36025cb8abc8
CaptJackSparrow21 <blockbustersanket211@gmail.com>
2026-09-25 12:46:19 +0530

    Auto-sync 2026-09-25 12:46

--- HackerRank/Python/Maximize_It.py
+++ HackerRank/Python/Maximize_It.py
@@ -1,2 +1,15 @@
 from itertools import product
 k, m = map(int, input().split())
+lists = []
+
+for _ in range(k) :
+    row = list(map(int, input().split()))
+    lists.append(row[1:])
+
+ans = 0
+
+for combination in product(*lists) :
+    value = sum(x * x for x in combination) % m
+    ans = max(ans, value)
+
+print(ans)
