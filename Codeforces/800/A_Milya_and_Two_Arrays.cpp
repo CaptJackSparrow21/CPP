b8f440771938ffaa7ca03988304f41853c2600fc
CaptJackSparrow21 <blockbustersanket211@gmail.com>
2026-09-26 19:57:26 +0530

    Auto-sync 2026-09-26 19:57

---
+++ Codeforces/800/A_Milya_and_Two_Arrays.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        vector<int> a(n), b(n);
+        for(int &i : a) cin >> i;
+        for(int &i : b) cin >> i;
+
+        set<int> sta, stb;
+        for(int i : a)
+            sta.insert(i);
+        for(int i : b)
+            stb.insert(i);
+
+        if(sta.size() == 1 || stb.size() == 1)
+            cout << "NO\n";
+
+    }
+
+    return 0;
+}
--- LeetCode/1807.evaluate-the-bracket-pairs-of-a-string.cpp
+++ LeetCode/1807.evaluate-the-bracket-pairs-of-a-string.cpp
@@ -8,7 +8,28 @@
 class Solution {
 public:
     string evaluate(string s, vector<vector<string>>& knowledge) {
-        
+        unordered_map<string, string> map;
+        for(auto &k : knowledge)
+            map[k[0]] = k[1];
+
+        string ans, key;
+        bool inside = false;
+
+        for(char c : s) {
+            if(c == '(') {
+                inside = true;
+                key = "";
+            }
+            else if(c == ')') {
+                inside = false;
+                ans += map.count(key) ? map[key] : "?";
+            }
+            else if(inside)
+                key += c;
+            else 
+                ans += c;
+        }
+        return ans;
     }
 };
 // @lc code=end
