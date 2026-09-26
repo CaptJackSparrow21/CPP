1c70ec4ca837e042caf02521c714bf233dc964ca
CaptJackSparrow21 <blockbustersanket211@gmail.com>
2026-09-26 23:13:34 +0530

    Auto-sync 2026-09-26 23:13

--- Striver/Strivers A2Z DSA Sheet/16. Dynamic Programming [Patterns and Problems]/8. MCM DP | Partition DP/ Burst balloons.cpp
+++ Striver/Strivers A2Z DSA Sheet/16. Dynamic Programming [Patterns and Problems]/8. MCM DP | Partition DP/ Burst balloons.cpp
@@ -4,11 +4,29 @@
 using namespace std;
 #define int long long
 
-//
+//TC = O(n^3) && SC = O(n^2)
 class Solution {
 public:
     int maxCoins(vector<int> &nums) {
-        
+        int n = nums.size();
+        nums.insert(nums.begin(), 1);
+        nums.push_back(1);
+
+        vector<vector<int>> dp(n + 2, vector<int> (n + 2, 0));
+
+        for(int len=2; len<=n+1; len++) {
+            for(int l=0; l+len<n+2; l++) {
+                int r = l + len;
+
+                for(int k=l+1; k<r; k++) {
+                    dp[l][r] = max(dp[l][r],
+                        dp[l][k] + dp[k][r] +
+                        nums[l] * nums[k] * nums[r]);
+                }
+            }
+        }
+
+        return dp[0][n+1];
     }
 };
 
